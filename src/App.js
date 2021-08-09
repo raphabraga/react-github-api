9266611980ff8b15e7d9cf2c43a5bb80dea53f0d
raphabraga <raphael-braga@hotmail.com>
2021-08-09 18:18:15 -0300

    used function createGlobalStyle from styled-components in order to reset default CSS styles. It was used code from destyle.css (by nicolas-cusan)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Layout from "./components/Layout";
 import Profile from "./components/Profile";
+import ResetCSS from "./global/ResetCSS";
 const App = () => {
   return (
     <main>
+      <ResetCSS />
       <Layout>
         <Profile />
         <div>Repositories</div>
